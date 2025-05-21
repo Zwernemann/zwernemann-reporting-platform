e80c58741864206c22322598244824e62027eca2
Martin Zwernemann <martin@zwernemann.de>
2025-05-21 12:56:10 +0200

    added Module to JSReport

---
+++ extensions/studio-ai/extension.js
@@ -0,0 +1,3 @@
+module.exports = function (reporter, definition) {
+  reporter.logger.info('studio-ai extension initialized');
+};
---
+++ extensions/studio-ai/jsreport.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'studio-ai',
+  main: './extension.js',
+  requires: {
+    core: '4.x.x',
+    studio: '4.x.x'
+  }
+}
